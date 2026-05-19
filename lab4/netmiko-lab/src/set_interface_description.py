78fe9ac7e6dcec3c604df6474b9990cb90cd160a
Vineela95 <vn616776@dal.ca>
2026-05-19 16:18:48 -0300

    Lab4 tasks finished

---
+++ lab4/netmiko-lab/src/set_interface_description.py
@@ -0,0 +1,55 @@
+#from netmiko import Netmiko
+#
+#devices = [
+ #   {
+  #      "device_type": "cisco_ios",
+   #     "ip": "192.168.1.101",
+    #    "username": "student",
+     #   "password": "Meilab123",
+     #   "port": "22",
+    #}
+#]
+#
+#description = "Description set with Netmiko"
+#
+#description_config = [
+ #   "interface GigabitEthernet3",
+  #  f"description {description}"
+#]
+#
+#for device in devices:
+ #   net_connect = Netmiko(**device)
+#
+ #   output = net_connect.send_config_set(description_config)
+#
+ #   print(output)
+
+#    net_connect.disconnect()
+
+
+
+
+
+
+
+from netmiko import Netmiko
+
+device = {
+    "device_type": "cisco_ios",
+    "ip": "192.168.1.101",
+    "username": "student",
+    "password": "Meilab123",
+    "port": "22"
+}
+
+commands = [
+    "interface Loopback100",
+    "description Created using Netmiko",
+    "ip address 100.100.100.100 255.255.255.255",
+    "no shutdown"
+]
+
+net_connect = Netmiko(**device)
+output = net_connect.send_config_set(commands)
+print(output)
+net_connect.disconnect()
